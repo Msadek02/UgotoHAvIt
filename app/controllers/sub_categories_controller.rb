6ac2c5662bad28aa4f59829a7fea8996a186b2eb
Msadek02 <Thunder_axe_90@hotmail.com>
2017-01-03 15:44:18 -0500

    adding subcategory controller

---
+++ app/controllers/sub_categories_controller.rb
@@ -0,0 +1,13 @@
+class SubCategoriesController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def update
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'sub_categories/index'
+
+  get 'sub_categories/new'
+
+  get 'sub_categories/edit'
+
+  get 'sub_categories/update'
+
   devise_for :users
 root "homes#index"
 
